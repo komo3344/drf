1b3c85a270c6bd7deef64793d1a34ce12fc69e85
wnsdud3119 <jy940608@naver.com>
2019-09-19 11:43:52 +0900

    들여쓰기

--- drfx/front-react/src/pages/Posts.js
+++ drfx/front-react/src/pages/Posts.js
@@ -3,216 +3,216 @@ import { Link } from 'react-router-dom';
 import { URL, Write } from 'pages';
 
 const style = {
-    image: {
-        border: '1px solid #ccc',
-        background: '#fefefe',
-    },
+  image: {
+    border: '1px solid #ccc',
+    background: '#fefefe',
+  },
 };
 class Posts extends Component {
-    state = {
-        posts: [],
-        editing : false,
-        writeShowResults: false
-    };
+  state = {
+    posts: [],
+    editing: false,
+    writeShowResults: false
+  };
 
-    handle_change = e => {
-        const name = e.target.name;
-        const value = e.target.value;
-        this.setState(prevstate => {
-          const newState = { ...prevstate };
-          newState[name] = value;
-          return newState;
-        });
-      };
+  handle_change = e => {
+    const name = e.target.name;
+    const value = e.target.value;
+    this.setState(prevstate => {
+      const newState = { ...prevstate };
+      newState[name] = value;
+      return newState;
+    });
+  };
 
-    handleDelete = url => {
-        fetch(url, {
-            method: 'DELETE',
-            headers: {
-                Authorization: `JWT ${localStorage.getItem('token')}`
-            }
-        }).then(() => {
-            this.postlist()
-        })
-    }
-    async postlist() {
-        try {
-            const res = await fetch(URL.posts);
-            const posts = await res.json();
-            this.setState({
-                posts
-            });
-        } catch (e) {
-            console.log(e);
-        }
-    }
-
-    handleUpdate = (id) => {
-        this.setState({
-            editing : !this.state.editing
-        })
-        fetch(`${URL.posts}${id}/`)
-        .then(res => res.json())
-        .then(json => {
-            this.setState({
-                id : id,
-                title : json.title,
-                content : json.content,
-                image : json.image,
-                
-            })
-        })
-        
-    }
-
-    handleModify = (e, data) => {
-        this.setState({
-            editing : !this.state.editing
-        })
-        e.preventDefault();
-        var form_data = new FormData();
-        form_data.append('image', data.image);
-        form_data.append('title', data.title);
-        form_data.append('content', data.content);
-
-        fetch(`${URL.posts}${this.state.id}/`,{
-            method: 'PUT',
-            headers: {
-                Authorization: `JWT ${localStorage.getItem('token')}`
-            },
-            body: form_data
-        }).then(() =>{
-            this.postlist()
-        })
-        
-    }
-
-    async componentDidMount() {
-        try {
-            const res = await fetch(URL.posts);
-            const posts = await res.json();
-            this.setState({
-                posts
-            });
-        } catch (e) {
-            console.log(e);
-        }
-    }
-    handle_write = (e, data) => {
-        e.preventDefault();
-        var form_data = new FormData();
-
-        form_data.append('image', data.image);
-        form_data.append('title', data.title);
-        form_data.append('content', data.content);      
-
-        fetch(URL.posts, {
-            method: 'POST',
-            headers: {
-                Accept : '*/*',
-                Authorization: `JWT ${localStorage.getItem('token')}`
-            },
-            body: form_data
-            
-        })
-            .then(res => {
-                res.json()
-            })
-            .then(json => {
-                this.setState({
-                    logged_in: true,
-                    displayed_form: '',
-                });
-            }).then(()=>{
-                this.postlist()
-            })
-    }
-    
-    onClick = () =>{
-        this.setState({
-            writeShowResults : !this.state.writeShowResults
-        })
-    }
-    handleImageChange = e => {
-        this.setState({
-          image: e.target.files[0]
-        })
+  handleDelete = url => {
+    fetch(url, {
+      method: 'DELETE',
+      headers: {
+        Authorization: `JWT ${localStorage.getItem('token')}`
       }
-    render() {
-        return (
-            <div>
-                <div>
-                <input type="submit" value="게시물 작성" onClick={this.onClick} />
-                { this.state.writeShowResults ? <Write handle_write={this.handle_write} /> : null }
-                </div>
-
-                <div>
-                {this.state.editing
-                    ? (
-                        <div>
-                            <h5>수정중</h5>
-                            <form onSubmit={(e) => {this.handleModify(e, this.state)}}>
-                                <label htmlFor="title">title</label>
-                                <input
-                                type="text"
-                                name="title"
-                                value={this.state.title}
-                                onChange={this.handle_change}
-                                />
-                                <label htmlFor="content">content</label>
-                                <input
-                                type="text"
-                                name="content"
-                                value={this.state.content}
-                                onChange={this.handle_change}
-                                /><br />
-                                <input
-                                required
-                                ref="file" //필요
-                                id="image" 
-                                accept="image/*"
-                                type="file"
-                                name="image"
-                                onChange={this.handleImageChange}
-                                />
-                                <input type="submit" />
-                            </form>
-                        </div>
-                        )
-                    : (
-                        <div>
-                            <h5>원래작업</h5>        
-                            {this.state.posts.map(item =>
-                                (
-                                    <div key={item.url}>
-                                        <h4>제목 : {item.title}</h4>
-                                        <p>내용 : {item.content}</p>
-                                        <p>작성자 : {item.owner}</p>
-                                        <p>생성일 : {item.created_at}</p>
-                                        <img
-                                         src={item.image}
-                                         alt='No img'
-                                         height={240}
-                                         width={240}
-                                         style={style.image
-                                        }
-                                        /><br />
-                           
-                                        <button onClick={() => {this.handleDelete(item.url)}}>삭제</button>
-                                        <button onClick={() => {this.handleUpdate(item.id)}}>수정</button>
-                                        <br /><br />
-                                    </div>
-                                )
-                            )
-                }           
-                <p><Link to='/'>Home</Link></p>
-                        </div>
-                        )
-                }
-                </div>
-                
-            </div>
-        );
+    }).then(() => {
+      this.postlist()
+    })
+  }
+  async postlist() {
+    try {
+      const res = await fetch(URL.posts);
+      const posts = await res.json();
+      this.setState({
+        posts
+      });
+    } catch (e) {
+      console.log(e);
     }
+  }
+
+  handleUpdate = (id) => {
+    this.setState({
+      editing: !this.state.editing
+    })
+    fetch(`${URL.posts}${id}/`)
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          id: id,
+          title: json.title,
+          content: json.content,
+          image: json.image,
+
+        })
+      })
+
+  }
+
+  handleModify = (e, data) => {
+    this.setState({
+      editing: !this.state.editing
+    })
+    e.preventDefault();
+    var form_data = new FormData();
+    form_data.append('image', data.image);
+    form_data.append('title', data.title);
+    form_data.append('content', data.content);
+
+    fetch(`${URL.posts}${this.state.id}/`, {
+      method: 'PUT',
+      headers: {
+        Authorization: `JWT ${localStorage.getItem('token')}`
+      },
+      body: form_data
+    }).then(() => {
+      this.postlist()
+    })
+
+  }
+
+  async componentDidMount() {
+    try {
+      const res = await fetch(URL.posts);
+      const posts = await res.json();
+      this.setState({
+        posts
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  handle_write = (e, data) => {
+    e.preventDefault();
+    var form_data = new FormData();
+
+    form_data.append('image', data.image);
+    form_data.append('title', data.title);
+    form_data.append('content', data.content);
+
+    fetch(URL.posts, {
+      method: 'POST',
+      headers: {
+        Accept: '*/*',
+        Authorization: `JWT ${localStorage.getItem('token')}`
+      },
+      body: form_data
+
+    })
+      .then(res => {
+        res.json()
+      })
+      .then(json => {
+        this.setState({
+          logged_in: true,
+          displayed_form: '',
+        });
+      }).then(() => {
+        this.postlist()
+      })
+  }
+
+  onClick = () => {
+    this.setState({
+      writeShowResults: !this.state.writeShowResults
+    })
+  }
+  handleImageChange = e => {
+    this.setState({
+      image: e.target.files[0]
+    })
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          <input type="submit" value="게시물 작성" onClick={this.onClick} />
+          {this.state.writeShowResults ? <Write handle_write={this.handle_write} /> : null}
+        </div>
+
+        <div>
+          {this.state.editing
+            ? (
+              <div>
+                <h5>수정중</h5>
+                <form onSubmit={(e) => { this.handleModify(e, this.state) }}>
+                  <label htmlFor="title">title</label>
+                  <input
+                    type="text"
+                    name="title"
+                    value={this.state.title}
+                    onChange={this.handle_change}
+                  />
+                  <label htmlFor="content">content</label>
+                  <input
+                    type="text"
+                    name="content"
+                    value={this.state.content}
+                    onChange={this.handle_change}
+                  /><br />
+                  <input
+                    required
+                    ref="file" //필요
+                    id="image"
+                    accept="image/*"
+                    type="file"
+                    name="image"
+                    onChange={this.handleImageChange}
+                  />
+                  <input type="submit" />
+                </form>
+              </div>
+            )
+            : (
+              <div>
+                <h5>원래작업</h5>
+                {this.state.posts.map(item =>
+                  (
+                    <div key={item.url}>
+                      <h4>제목 : {item.title}</h4>
+                      <p>내용 : {item.content}</p>
+                      <p>작성자 : {item.owner}</p>
+                      <p>생성일 : {item.created_at}</p>
+                      <img
+                        src={item.image}
+                        alt='No img'
+                        height={240}
+                        width={240}
+                        style={style.image
+                        }
+                      /><br />
+
+                      <button onClick={() => { this.handleDelete(item.url) }}>삭제</button>
+                      <button onClick={() => { this.handleUpdate(item.id) }}>수정</button>
+                      <br /><br />
+                    </div>
+                  )
+                )
+                }
+                <p><Link to='/'>Home</Link></p>
+              </div>
+            )
+          }
+        </div>
+
+      </div>
+    );
+  }
 }
 export default Posts;
--- drfx/front-react/src/shared/App.js
+++ drfx/front-react/src/shared/App.js
@@ -4,122 +4,122 @@ import { URL, Posts, Users, Nav, SignupForm, LoginForm } from 'pages';
 
 
 class App extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            displayed_form: '',
-            logged_in: localStorage.getItem('token') ? true : false,
-            username: ''
-        };
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      displayed_form: '',
+      logged_in: localStorage.getItem('token') ? true : false,
+      username: ''
+    };
+  }
 
-    componentDidMount() {
-        if (this.state.logged_in) {
-            fetch(`${URL.userlist}${localStorage.getItem('id')}/`, {
-                headers: {
-                    Authorization: `JWT ${localStorage.getItem('token')}`
-                }
-            })
-                .then(res => res.json())
-                .then(json => {
-                    this.setState({ username: json.username });
-                });
+  componentDidMount() {
+    if (this.state.logged_in) {
+      fetch(`${URL.userlist}${localStorage.getItem('id')}/`, {
+        headers: {
+          Authorization: `JWT ${localStorage.getItem('token')}`
         }
-    }
-    handle_login = (e, data) => {
-        e.preventDefault();
-        fetch(URL.login, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        })
-            .then(res => res.json())
-            .then(json => {
-                localStorage.setItem('token', json.token);
-                localStorage.setItem('id', json.user.id);
-                this.setState({
-                    logged_in: true,
-                    displayed_form: '',
-                    username: json.user.username
-                });
-            });
-    };
-
-    
-    handle_signup = (e, data) => {
-        e.preventDefault();
-        fetch(URL.signup, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'X-CSRFToken' : document.querySelector('meta[name="csrf-token"]')
-            },
-            body: JSON.stringify(data)
-        })
-            .then(res => res.json())
-            .then(json => {
-                localStorage.setItem('token', json.token);
-                this.setState({
-                    logged_in: true,
-                    displayed_form: '',
-                    username: data.username
-                });
-            });
-    };
-
-    handle_logout = () => {
-        localStorage.removeItem('token');
-        this.setState({ logged_in: false, username: '' });
-    };
-
-    display_form = form => {
-        this.setState({
-            displayed_form: form
+      })
+        .then(res => res.json())
+        .then(json => {
+          this.setState({ username: json.username });
         });
-    };
-
-    render() {
-        let form;
-        switch (this.state.displayed_form) {
-            case 'login':
-                form = <LoginForm handle_login={this.handle_login} />;
-                break;
-            case 'signup':
-                form = <SignupForm handle_signup={this.handle_signup} />;
-                break;
-            default:
-                form = null;
-        }
-        return (
-            <div>
-                <Nav
-                    logged_in={this.state.logged_in}
-                    display_form={this.display_form}
-                    handle_logout={this.handle_logout}
-                />
-                {form}
-                <h3>
-                    {this.state.logged_in
-                        ? (
-                        <div >
-                            <h4>Hello, {this.state.username}</h4>
-                            <p><Link to='/users'>User List</Link></p>
-                            <p><Link to='/posts'>posts List</Link></p>
-                            <Route path="/posts" component={Posts} />
-                            <Route path="/users" component={Users} />
-                        </div>
-                        )
-                        : 'Please Log In'
-                    }
-                </h3>
-                
-                
-            </div>
-        );
-
     }
+  }
+  handle_login = (e, data) => {
+    e.preventDefault();
+    fetch(URL.login, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem('token', json.token);
+        localStorage.setItem('id', json.user.id);
+        this.setState({
+          logged_in: true,
+          displayed_form: '',
+          username: json.user.username
+        });
+      });
+  };
+
+
+  handle_signup = (e, data) => {
+    e.preventDefault();
+    fetch(URL.signup, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'X-CSRFToken': document.querySelector('meta[name="csrf-token"]')
+      },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(json => {
+        localStorage.setItem('token', json.token);
+        this.setState({
+          logged_in: true,
+          displayed_form: '',
+          username: data.username
+        });
+      });
+  };
+
+  handle_logout = () => {
+    localStorage.removeItem('token');
+    this.setState({ logged_in: false, username: '' });
+  };
+
+  display_form = form => {
+    this.setState({
+      displayed_form: form
+    });
+  };
+
+  render() {
+    let form;
+    switch (this.state.displayed_form) {
+      case 'login':
+        form = <LoginForm handle_login={this.handle_login} />;
+        break;
+      case 'signup':
+        form = <SignupForm handle_signup={this.handle_signup} />;
+        break;
+      default:
+        form = null;
+    }
+    return (
+      <div>
+        <Nav
+          logged_in={this.state.logged_in}
+          display_form={this.display_form}
+          handle_logout={this.handle_logout}
+        />
+        {form}
+        <h3>
+          {this.state.logged_in
+            ? (
+              <div >
+                <h4>Hello, {this.state.username}</h4>
+                <p><Link to='/users'>User List</Link></p>
+                <p><Link to='/posts'>posts List</Link></p>
+                <Route path="/posts" component={Posts} />
+                <Route path="/users" component={Users} />
+              </div>
+            )
+            : 'Please Log In'
+          }
+        </h3>
+
+
+      </div>
+    );
+
+  }
 }
 export default App;
 
